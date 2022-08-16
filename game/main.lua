06e1cf3d32a4004c96b8e4d76d458a7cc84ee089
wewewer123 <iverhoeff@outlook.com>
2022-08-16 17:34:04 +0200

    cleaned up main.lua a bit

--- game/main.lua
+++ game/main.lua
@@ -20,9 +20,6 @@ function love.gamepadpressed(joystick, button)
 	if button == "a" then
 		love.keyboard.setTextInput( enable )
 	end
-	if button == "b" then
-		
-	end
 end
 
 function love.textinput(key)
@@ -30,7 +27,7 @@ function love.textinput(key)
 end
 	
 function love.draw()
-	love.graphics.draw(TestImage, 0, 0)
+love.graphics.draw(TestImage, 0, 0)
 love.graphics.print(script,0,0)
 EditedScript = tostring(love.filesystem.read("script.txt", MoreLocation-1))
 --love.graphics.print(script:gsub(EditedScript))
