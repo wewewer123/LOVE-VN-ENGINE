fe2d2a8a94546001751eab6eb319af67d7ba836e
wewewer123 <iverhoeff@outlook.com>
2022-08-17 21:09:18 +0200

    Added A queston System

--- game/main.lua
+++ game/main.lua
@@ -1,6 +1,7 @@
 function love.load()
 	UneditedScript = love.filesystem.read("script.txt")
 	UneditedImageList = love.filesystem.read("image.txt")
+	QuestionList = love.filesystem.read("questions.txt")
 	Song = love.audio.newSource("nitizyou1.mp3", "stream")
 	Line = 1;
 	LineString = tostring(Line)
@@ -13,7 +14,7 @@ function love.load()
 
 	MoreLocation = UneditedScript:find("1")
 	MoreLocationString = tostring(MoreLocation)
-	OldMoreLocation = 0
+	OldMoreLocation = 2
 	script = UneditedScript.sub(UneditedScript, OldMoreLocation, MoreLocation-1)
 	OldMoreLocation = MoreLocation
 end
@@ -35,6 +36,12 @@ function love.gamepadpressed(joystick, button)
 			love.audio.play(Song)
 		end
 	else
+	if button == "a" then
+		QuestionAwnser = "yes"
+	end
+	if button == "b" then
+		QuestionAwnser = "no"
+	end
 	DrawNext()
 	end
 end
@@ -50,10 +57,25 @@ function DrawNext()
 
 	Image = love.graphics.newImage(ImageName)
 
-	MoreLocation = UneditedScript:find(LineString)
-	MoreLocationString = tostring(MoreLocation)
-	script = UneditedScript.sub(UneditedScript, OldMoreLocation+1, MoreLocation-1)
-	OldMoreLocation = MoreLocation
+	if QuestionList:find(LineString) then
+		OldMoreLocation = UneditedScript:find(" "..LineString..".n ")
+		if QuestionAwnser == "yes" then
+			MoreLocation = UneditedScript:find(" "..LineString..".y ")
+			OldMoreLocation = UneditedScript:find(" "..LineString..".y.s ")
+			script = UneditedScript.sub(UneditedScript, OldMoreLocation+7, MoreLocation-1)
+		end
+		if QuestionAwnser == "no" then
+			MoreLocation = UneditedScript:find(" "..LineString..".n ")
+			OldMoreLocation = UneditedScript:find(" "..LineString..".n.s ")
+			script = UneditedScript.sub(UneditedScript, OldMoreLocation+7, MoreLocation-1)
+		end
+	else
+		MoreLocation = UneditedScript:find(" "..LineString.." ")
+		MoreLocationString = tostring(MoreLocation)
+		script = UneditedScript.sub(UneditedScript, OldMoreLocation+4, MoreLocation-1)
+		OldMoreLocation = MoreLocation
+	end
+	QuestionAwnser = "blank"
 end
 
 function love.textinput(key)
