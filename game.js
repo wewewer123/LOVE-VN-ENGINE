8de4651304d023c9fc4bf6e4d3017128137103aa
wewewer123 <74647510+wewewer123@users.noreply.github.com>
2024-12-11 12:34:23 +0000

    Deploying to pages from @ wewewer123/LOVE-VN-ENGINE@56edcd6bb76459b966b683e31359b1f5c6f8aa78 🚀

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"9301b442-9257-4f8c-b3ee-83d53db879d8","remote_package_size":10859869,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":10859869,"audio":false}]});
+  loadPackage({"package_uuid":"68e3c85a-579c-4817-bc85-4c50f2627d03","remote_package_size":10859869,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":10859869,"audio":false}]});
 
 })();
